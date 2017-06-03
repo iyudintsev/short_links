b3def7b190e47a3677a9864c173757b98c53d019
iyudintsev <ivan.yudintsev18@gmail.com>
2017-06-04 00:40:46 +0300

    [forms] add strip filter

--- application/forms.py
+++ application/forms.py
@@ -4,4 +4,5 @@ from wtforms.validators import URL
 
 
 class GenerateShortUrlForm(FlaskForm):
-    url = StringField('Please, input url', validators=[URL(require_tld=False)])
+    url = StringField('Please, input url', validators=[URL(require_tld=False)],
+                      filters=[lambda x: x.strip() if x else None])
